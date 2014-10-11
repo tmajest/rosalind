a290ebd50528b6c5c2907915c0ae5e110694e3b0
Tony Majestro <tonymajestro@gmail.com>
2014-10-10 20:08:44 -0700

    Added fasta module for parsing fasta files

--- gc/gc.py
+++ gc/gc.py
@@ -1,24 +1,13 @@
 # http://rosalind.info/problems/gc/
 
-import sys
+import fasta
 
-def getGcContent(s):
+def gcContent(s):
     return float(sum(1 for c in s if c == 'G' or c == 'C')) / len(s)
 
-def parseInput():
-    lines = [line.rstrip() for line in sys.stdin.readlines()]
-    sId = lines[0][1:]
-    s = []
+entries = fasta.parse()
+contents = ((d, gcContent(s)) for d, s in entries)
+desc, maxGcContent = max(contents, key=lambda x: x[1])
 
-    for line in lines[1:]:
-        if line[0] == '>':
-            yield sId, getGcContent(''.join(s))
-            sId, s = line[1:], []
-        else:
-            s.append(line)
-
-    yield sId, getGcContent(''.join(s))
-
-sId, s = max([entry for entry in parseInput()], key=lambda x: x[1])
-print sId
-print s * 100
+print desc 
+print maxGcContent * 100
